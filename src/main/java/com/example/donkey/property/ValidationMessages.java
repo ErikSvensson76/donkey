d7e510bab536ff236c88eb5e08f1d4eeb8e2d2ba
ErikSvensson76 <eosvensson@gmail.com>
2022-10-10 21:04:05 +0200

    Added static validation messages in ValidationMessages

---
+++ src/main/java/com/example/donkey/property/ValidationMessages.java
@@ -0,0 +1,9 @@
+package com.example.donkey.property;
+
+public class ValidationMessages {
+    public static final String LIST_EMPTY = "List must not be empty";
+    public static final String INVALID_DIRECTORY = "Invalid directory";
+    public static final String DIRECTORY_EMPTY = "Filename must not be empty";
+    public static final String INVALID_FILENAME = "Invalid filename";
+    public static final String FILENAME_EMPTY = "Filename must not be empty";
+}
